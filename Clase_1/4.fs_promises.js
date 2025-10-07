e4c44214b84275c8b92b4a04b80915e09e693df4
DanielGomez23 <danielg20052@gmail.com>
2025-10-07 17:41:11 -0500

    buil: dependencias

--- Clase_1/4.fs_promises.js
+++ Clase_1/4.fs_promises.js
@@ -13,3 +13,26 @@ fs.readFile('./README.md', 'utf-8')
 .then(text => {
     console.log('segundo texto:', text)
 })
+
+// function getSumNum(a, b) {
+//   const customPromise = new Promise((resolve, reject) => {
+//     const sum = a + b;
+
+//     if(sum <= 5){
+//       resolve("Let's go!!")
+//     } else {
+//       reject(new Error('Oops!.. Number must be less than 5'))
+//     }
+//   })
+
+//   return customPromise
+// }
+
+// // consuming the promise
+// getSumNum(1, 3).then(data => {
+//   console.log(data)
+// })
+// .catch(err => {
+//   console.log(err)
+// })
+
--- Clase_2/3.express.js
+++ Clase_2/3.express.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const ditto = require('./pokemon/ditto.json')
- 
-const PORT = process.env.PORT ?? 1234
+const express = require("express");
+const ditto = require("./pokemon/ditto.json");
 
-const app = express()
-app.disable('x-powered-by')
+const PORT = process.env.PORT ?? 1234;
 
-app.use(express.json())
+const app = express();
+app.disable("x-powered-by");
+
+app.use(express.json());
 app.use((req, res, next) => {
-    if (req.method !== 'POST') return next()
-    if(req.headers['content-type'] !== 'application/json') return next() 
+  if (req.method !== "POST") return next();
+  if (req.headers["content-type"] !== "application/json") return next();
 
-      let body = ''
+  let body = "";
 
-    req.on('data', chunk => {
-        body += chunk.toString()
-    })
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
 
-    req.on('end', () => {
-        const data = JSON.parse(body)
-        data.timestamp = Date.now()
-        req.body = data
-        next()
-    })
-})
+  req.on("end", () => {
+    const data = JSON.parse(body);
+    data.timestamp = Date.now();
+    req.body = data;
+    next();
+  });
+});
 
-app.get('/pokemon/ditto', (req, res) => {
-    res.json(ditto)
-})
+app.get("/pokemon/ditto", (req, res) => {
+  res.json(ditto);
+});
 
-app.post('/pokemon', (req, res) => {
-    res.status(201).json(req.body)
-})
- 
+app.post("/pokemon", (req, res) => {
+  res.status(201).json(req.body);
+});
+
 app.use((req, res) => {
-    res.status(404).send('<h1>404</h1>')
-})
+  res.status(404).send("<h1>404</h1>");
+});
 app.listen(PORT, () => {
-    console.log(`Server listening at port http://localhost:${PORT}`)
-})
+  console.log(`Server listening at port http://localhost:${PORT}`);
+});
