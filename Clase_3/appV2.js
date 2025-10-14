fdf34eae56375162ee06c6e07fa3405d06cbfb46
DanielGomez23 <danielg20052@gmail.com>
2025-10-14 13:11:30 -0500

    feat: solución de cors

--- Clase_3/appV2.js
+++ Clase_3/appV2.js
@@ -3,15 +3,44 @@ import crypto from "node:crypto";
 import movies from "./movies.json" with { type: "json" };
 import {validateMovie, validatePartialMovie} from "./movies.js"
 import { error } from "node:console";
+import cors from "cors";
+
 
 const app = express();
 app.use(express.json())
+
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com',
+      'https://midu.dev'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
 app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
   res.json({ message: "hola mundo" });
 });
 
+const ACCEPTED_ORIGINS = [
+  "http://localhost:8080",
+  "http://localhost:1234",
+  "https://movies.com"
+]
 //Todos los recursos que sean MOVIES se identifican como /movies
 app.get("/movies", (req, res) => {
   const { genre } = req.query
@@ -48,6 +77,18 @@ const result = validateMovie(req.body)
     res.status(201).json(newMovie) //actualizar caché del cliente
 })
  
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
 
 app.patch("/movies/:id", (req, res) => {
     const result = validatePartialMovie(req.body)
@@ -71,8 +112,9 @@ app.patch("/movies/:id", (req, res) => {
 
     return res.json(updateMovie)
 })
+
 const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
-  console.log(`SERVER LISTENING AT PORT https://localhost:${PORT}`);
+  console.log(`SERVER LISTENING AT PORT http://localhost:${PORT}`);
 });
